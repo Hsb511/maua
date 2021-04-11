c4484f572daf5ada141e3f381b3778bbf63505f4
Hsb511 <hugo-selle@hotmail.fr>
2021-04-11 18:36:41 +0200

    Centering upload list, custom upload and remove all buttons, showing file size

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MatFileUploadQueueComponent } from 'angular-material-fileupload';
 
 @Component({
   selector: 'app-upload',
@@ -6,10 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent {
+  @ViewChild('fileUploadQueue') myFileUploadQueue?: ElementRef;
 
   constructor() { }
 
-  deleteFile() {
-
+  deleteFile(file: File) {
+    if (this.myFileUploadQueue instanceof MatFileUploadQueueComponent) {
+      const files = this.myFileUploadQueue.files;
+      console.log(files);
+      const index = files.indexOf(file, 0);
+      files.splice(index, 1);
+    }
   }
 }
