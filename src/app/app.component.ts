8036da3d50d291752569119ec274a895e2bbf733
Hsb511 <hugo-selle@hotmail.fr>
2021-04-18 19:22:40 +0200

    Handling dark theme changes + some cleaning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ThemeService } from './theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  isDarkTheme?: Observable<boolean>;
+
+  constructor(private themeService: ThemeService) { }
+
+  ngOnInit() {
+    this.isDarkTheme = this.themeService.isDarkTheme;
+  }
 }
--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { ThemeService } from '../theme.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -12,15 +13,18 @@ export class ToolbarComponent {
   selectedLanguage = 'fr';
   selectedTheme = 'light_mode';
 
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService,
+    private themeService: ThemeService) {
     this.availableLangauges = translate.getLangs();
   }
 
   changeMode() {
     if (this.selectedTheme == 'light_mode') {
       this.selectedTheme = 'dark_mode';
+      this.themeService.setDarkTheme(true);
     } else {
       this.selectedTheme = 'light_mode';
+      this.themeService.setDarkTheme(false);
     }
   }
 }
