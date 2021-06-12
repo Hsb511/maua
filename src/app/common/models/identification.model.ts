ba87fbbb5d8d69f173310935fdc287e8dda3d386
Laure SOUCHE <laure.souche@gmail.com>
2021-06-12 19:35:19 +0200

    First working version of service

---
+++ src/app/common/models/identification.model.ts
@@ -0,0 +1,49 @@
+export class IdentificationModel {
+  constructor(results: Result[]){
+    this.results = results;
+  }
+  results: Result[];
+}
+
+export class Result {
+  constructor(score: number, species: Specie){
+    this.score = score;
+    this.species = species;
+  }
+  score: number;
+  species: Specie;
+}
+
+class NamedObject {
+  constructor(
+    scientificNameWithoutAuthor: string,
+    scientificNameAuthorship: string,
+    scientificName: string
+  ){
+    this.scientificName = scientificName;
+    this.scientificNameAuthorship = scientificNameAuthorship;
+    this.scientificNameWithoutAuthor = scientificNameWithoutAuthor;
+  };
+  scientificNameWithoutAuthor: string;
+  scientificNameAuthorship: string;
+  scientificName: string;
+}
+
+export class Specie extends NamedObject {
+  constructor(
+    scientificNameWithoutAuthor: string,
+    scientificNameAuthorship: string,
+    scientificName: string,
+    genus: NamedObject,
+    family: NamedObject,
+    commonNames:string[]
+  ) {
+    super(scientificNameWithoutAuthor, scientificNameAuthorship, scientificName);
+    this.genus = genus;
+    this.family = family;
+    this.commonNames = commonNames;
+  }
+  genus: NamedObject;
+  family: NamedObject;
+  commonNames: string[];
+}
